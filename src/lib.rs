54b7e3529843c24b8ab91eff0f7e1424ec4d5a2c
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2025-11-11 20:46:29 +1100

    feat: search function

--- src/lib.rs
+++ src/lib.rs
@@ -38,6 +38,68 @@ impl Config {
     }
 }
 
+pub fn search<'a>(pattern: &str, input: &'a str) -> Vec<(usize, &'a str)> {
+    let mut matches: Vec<(usize, &str)> = vec![];
+    for (line_no, line) in input.lines().enumerate() {
+        if line.contains(pattern) {
+            matches.push((line_no + 1, line));
+        }
+    }
+    matches
+}
+
+#[test]
+fn search_test_matching_lines() {
+    let pattern = "testing";
+    let input = "\
+This is the testing
+kind of test
+where we do testing
+for the tests
+of testing";
+    let output = [
+        (1, "This is the testing"),
+        (3, "where we do testing"),
+        (5, "of testing"),
+    ];
+
+    assert_eq!(output, *search(pattern, input));
+}
+
+#[test]
+fn search_test_no_matches() {
+    let pattern = "nothing";
+    let input = "\
+This is the testing
+kind of test
+where we do testing
+for the tests
+of testing";
+    let output: Vec<(usize, &str)> = vec![];
+
+    assert_eq!(output, *search(pattern, input));
+}
+
+#[test]
+fn search_test_no_pattern() {
+    let pattern = "";
+    let input = "\
+This is the testing
+kind of test
+where we do testing
+for the tests
+of testing";
+    let output = [
+        (1, "This is the testing"),
+        (2, "kind of test"),
+        (3, "where we do testing"),
+        (4, "for the tests"),
+        (5, "of testing"),
+    ];
+
+    assert_eq!(output, *search(pattern, input));
+}
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum ConfigError {
     MissingPattern,
